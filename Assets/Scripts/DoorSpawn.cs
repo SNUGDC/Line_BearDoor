4f6bdcc03af35dd844f0f48f8022fbf5880012e5
Yang KM <yangkyeongmo@snu.ac.kr>
2017-01-15 17:35:08 +0900

    제대로 스와이프시 화살표 제거

--- Assets/Scripts/DoorSpawn.cs
+++ Assets/Scripts/DoorSpawn.cs
@@ -28,62 +28,68 @@ public class DoorSpawn : MonoBehaviour {
         SpawnWaves();
     }
 
-    void Update() {
-        SwipeDoor();
-    }
-
     public void SpawnWaves()
     {
         GameObject[] Door = new GameObject[3];
         Door[0] = leftDoor;
         Door[1] = rightDoor;
         Door[2] = upDoor;
-        
+
         if (hg.hunger > 0)
         {
-			newDoor = Instantiate(Door[Random.Range(0, 3)], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            newDoor = Instantiate(Door[Random.Range(0, 3)], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
         }
-        
+    }
+
+    void Update()
+    {
+        SwipeDoor();
     }
 
     void SwipeDoor()
     {
         if(newDoor != null)
         {
-            DestroyDoor(SwipeDirectionbyMouse.LEFT, "Left");
-            DestroyDoor(SwipeDirectionbyMouse.RIGHT, "Right");
-            DestroyDoor(SwipeDirectionbyMouse.UP, "Up");
+            CorrectlySwiped(SwipeDirectionbyMouse.LEFT, "Left");
+            CorrectlySwiped(SwipeDirectionbyMouse.RIGHT, "Right");
+            CorrectlySwiped(SwipeDirectionbyMouse.UP, "Up");
             //Destroy and AddScore if Correctly Swiped
 
-            DestroyDoorifIncorrect(SwipeDirectionbyMouse.LEFT, "Left");
-            DestroyDoorifIncorrect(SwipeDirectionbyMouse.RIGHT, "Right");
-            DestroyDoorifIncorrect(SwipeDirectionbyMouse.UP, "Up");
+            IncorrectlySwiped(SwipeDirectionbyMouse.LEFT, "Left");
+            IncorrectlySwiped(SwipeDirectionbyMouse.RIGHT, "Right");
+            IncorrectlySwiped(SwipeDirectionbyMouse.UP, "Up");
             //Destroy if Incorrectly Swiped
         }
     }
 
-    void DestroyDoor(SwipeDirectionbyMouse dir, string door)
+    void CorrectlySwiped(SwipeDirectionbyMouse dir, string door)
     {
-        if ((swipe.swipeDirection == dir) && (newDoor.name == door+"SwipeDoor(Clone)"))
+        if ((swipe.swipeDirection == dir) && (newDoor.name == door + "SwipeDoor(Clone)"))
         {
-            Debug.Log ("Correct!");
-            Destroy(newDoor);
-            swipe.directionChosen = false;
-            isCorrect = true;
+            Debug.Log("Correct!");
+            Destroy(GameObject.Find(door + "SwipeArrow"));
             AddScore();
-            SpawnWaves();
+            swipe.directionChosen = false;
+            swipe.swipeDirection = SwipeDirectionbyMouse.NONE;
+            DestroyDoor(door);
         }
     }
 
-    void DestroyDoorifIncorrect(SwipeDirectionbyMouse dir, string door)
+    void IncorrectlySwiped(SwipeDirectionbyMouse dir, string door)
     {
         if ((swipe.swipeDirection != dir) && (newDoor.name == door + "SwipeDoor(Clone)") && (swipe.swipeDirection != SwipeDirectionbyMouse.NONE))
         {
-            Debug.Log ("Fail");
-            Destroy(newDoor);
+            Debug.Log("Fail");
             swipe.directionChosen = false;
+            swipe.swipeDirection = SwipeDirectionbyMouse.NONE;
+        }
+    }
+
+    void DestroyDoor(string door)
+    {
+        if (isCorrect)
+        {
             isCorrect = false;
-            SpawnWaves();
         }
     }
 
@@ -113,3 +119,4 @@ public class DoorSpawn : MonoBehaviour {
     }
 }
 
+//update -> swipeDoor -> DestroyDoor -> AddScore -> newDoor.name == ~~~ -> UpdateScore
