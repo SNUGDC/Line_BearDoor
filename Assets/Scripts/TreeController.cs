5a4b7dfc5df7c4c0619dc53cf53b5b9a4baa2823
EQwon <bluetwintail@naver.com>
2017-01-23 20:08:19 +0900

    눈을 속이자!

---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    public Position TreePosition;
+
+    private float MoveSpeed;
+    private float MoveDistance;
+
+    public enum Position
+    {
+        Right,
+        Left
+    }
+
+    private void Start()
+    {
+        MoveSpeed = 2.8f;
+    }
+
+    void Update()
+    {
+        MoveDistance = MoveSpeed * Time.deltaTime;
+
+        switch (TreePosition)
+        {
+        case Position.Left:
+            transform.position = new Vector3 (transform.position.x + 4 * MoveDistance / 7, transform.position.y - MoveDistance, transform.position.z - 50 * MoveDistance / 7);
+            break;
+        case Position.Right:
+            transform.position = new Vector3 (transform.position.x - 4 * MoveDistance / 7, transform.position.y - MoveDistance, transform.position.z - 50 * MoveDistance / 7);
+            break;
+        }
+
+        if (transform.position.y < -2f)
+            Destroy (this.gameObject);
+    }
+}
