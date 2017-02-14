76fb7dfaaa8a47c5c03357b773a811e067426b65
Song <dydwo1127@gmail.com>
2017-02-14 20:49:08 +0900

    Hunger 스크립트에서 newDoor의 타입을 이름으로 확인하던 것 수정

--- Assets/Scripts/DoorComponent.cs
+++ Assets/Scripts/DoorComponent.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class DoorComponent : MonoBehaviour {
     public SwipeDirectionbyMouse direction = SwipeDirectionbyMouse.NONE;
     public GameObject arrow;
+    public bool isReverseDoor;
 }
