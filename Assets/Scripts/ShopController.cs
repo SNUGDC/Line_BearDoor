60983ec52fb831904b32806fbed4e0a8e82e89cb
EQwon <bluetwintail@naver.com>
2017-02-24 00:57:11 +0900

    살짝 샵 적용

--- Assets/Scripts/ShopController.cs
+++ Assets/Scripts/ShopController.cs
@@ -5,13 +5,21 @@ using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
+    public int[] BearPrice_Bb;
+    public int[] BearPrice_H;
+
     public Text BlueberryText;
     public Text HoneyText;
+
+    private int Blueberry;
+    private int Honey;
 
     private void Update()
     {
         BlueberryText.text = HowMuchBlueberryHave().ToString();
+        Blueberry = HowMuchBlueberryHave();
         HoneyText.text = HowMuchHoneyHave().ToString();
+        Honey = HowMuchHoneyHave();
     }
 
     private int HowMuchBlueberryHave()
@@ -44,8 +52,16 @@ public class ShopController : MonoBehaviour
         PlayerPrefs.SetInt("Honey", PlayerPrefs.GetInt("Honey") + 100);
     }
 
-    public void TryToBuy1stBearByBlueberry()
+    public void CanBuy1stBearByBlueberry()
     {
-
+        if (Blueberry >= BearPrice_Bb[0])
+        {
+            PlayerPrefs.SetInt("Blueberry", PlayerPrefs.GetInt("Blueberry") - BearPrice_Bb[0]);
+            Debug.Log("너는 첫번째 곰을 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 첫번째 곰을 사기에는 돈이 부족하다...");
+        }
     }
 }
