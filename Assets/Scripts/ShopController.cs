fd494a09fea715e3f8893b97fb6e57826ad51585
EQwon <bluetwintail@naver.com>
2017-02-24 01:37:21 +0900

    일단 버튼을 구현

--- Assets/Scripts/ShopController.cs
+++ Assets/Scripts/ShopController.cs
@@ -57,11 +57,134 @@ public class ShopController : MonoBehaviour
         if (Blueberry >= BearPrice_Bb[0])
         {
             PlayerPrefs.SetInt("Blueberry", PlayerPrefs.GetInt("Blueberry") - BearPrice_Bb[0]);
-            Debug.Log("너는 첫번째 곰을 샀다!");
+            Debug.Log("너는 첫번째 곰을 블루베리로 샀다!");
         }
         else
         {
-            Debug.Log("너는 첫번째 곰을 사기에는 돈이 부족하다...");
+            Debug.Log("너는 첫번째 곰을 사기에는 블루베리가 부족하다...");
+        }
+    }
+
+    public void CanBuy1stBearByHoney()
+    {
+        if (Honey >= BearPrice_H[0])
+        {
+            PlayerPrefs.SetInt("Honey", PlayerPrefs.GetInt("Honey") - BearPrice_H[0]);
+            Debug.Log("너는 첫번째 곰을 꿀로 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 첫번째 곰을 사기에는 꿀이 부족하다...");
+        }
+    }
+
+    public void CanBuy2stBearByBlueberry()
+    {
+        if (Blueberry >= BearPrice_Bb[1])
+        {
+            PlayerPrefs.SetInt("Blueberry", PlayerPrefs.GetInt("Blueberry") - BearPrice_Bb[1]);
+            Debug.Log("너는 두번째 곰을 블루베리로 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 두번째 곰을 사기에는 블루베리가 부족하다...");
+        }
+    }
+
+    public void CanBuy2stBearByHoney()
+    {
+        if (Honey >= BearPrice_H[1])
+        {
+            PlayerPrefs.SetInt("Honey", PlayerPrefs.GetInt("Honey") - BearPrice_H[1]);
+            Debug.Log("너는 두번째 곰을 꿀로 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 두번째 곰을 사기에는 꿀이 부족하다...");
+        }
+    }
+
+    public void CanBuy3stBearByBlueberry()
+    {
+        if (Blueberry >= BearPrice_Bb[2])
+        {
+            PlayerPrefs.SetInt("Blueberry", PlayerPrefs.GetInt("Blueberry") - BearPrice_Bb[2]);
+            Debug.Log("너는 세번째 곰을 블루베리로 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 세번째 곰을 사기에는 블루베리가 부족하다...");
+        }
+    }
+
+    public void CanBuy3stBearByHoney()
+    {
+        if (Honey >= BearPrice_H[2])
+        {
+            PlayerPrefs.SetInt("Honey", PlayerPrefs.GetInt("Honey") - BearPrice_H[2]);
+            Debug.Log("너는 세번째 곰을 꿀로 샀다!");
+        }
+        else
+        {
+            Debug.Log("너는 세번째 곰을 사기에는 꿀이 부족하다...");
+        }
+    }
+
+    public void CanBuyBear(int WhatBear, int BlueberryPrice, bool IsBlueberry)
+    {
+        switch (WhatBear)
+        {
+            case 1:
+                if (IsBlueberry)
+                {
+                    if (Blueberry >= BlueberryPrice)
+                    {
+                        PlayerPrefs.SetInt("Blueberry", Blueberry - BlueberryPrice);
+                    }
+                }
+                else
+                {
+                    if (Honey >= BlueberryPrice / 20)
+                    {
+                        PlayerPrefs.SetInt("Honey", Honey - BlueberryPrice / 20);
+                    }
+                }
+                break;
+            case 2:
+                if (IsBlueberry)
+                {
+                    if (Blueberry >= BlueberryPrice)
+                    {
+                        PlayerPrefs.SetInt("Blueberry", Blueberry - BlueberryPrice);
+                    }
+                }
+                else
+                {
+                    if (Honey >= BlueberryPrice / 20)
+                    {
+                        PlayerPrefs.SetInt("Honey", Honey - BlueberryPrice / 20);
+                    }
+                }
+                break;
+            case 3:
+                if (IsBlueberry)
+                {
+                    if (Blueberry >= BlueberryPrice)
+                    {
+                        PlayerPrefs.SetInt("Blueberry", Blueberry - BlueberryPrice);
+                    }
+                }
+                else
+                {
+                    if (Honey >= BlueberryPrice / 20)
+                    {
+                        PlayerPrefs.SetInt("Honey", Honey - BlueberryPrice / 20);
+                    }
+                }
+                break;
+            default:
+                Debug.Log("Something wrong with buying Bear");
+                break;
         }
     }
 }
